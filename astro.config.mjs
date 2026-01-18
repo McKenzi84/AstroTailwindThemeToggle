c1119aef8eb1dd4dcd030170527a8a2fd0edb3b6
McKenzi84 <smallbevelgear@gmail.com>
2026-01-18 19:30:13 +0100

    Config website base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  base: "/",
+  site: "https://mckenzi84.github.io/AstroTailwindThemeToggle",
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
